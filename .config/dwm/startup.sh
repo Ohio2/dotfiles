050cbf474f383f87558895c268c82f89c825f441
Ohio2 <sekmartyna4@gmail.com>
2022-04-30 23:12:24 +0200

    add dwm

---
+++ .config/dwm/startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+_here=$HOME/.config/dwm
+XDG_RUNTIME_DIR="/run/user/$(id -u)"
+PATH="$PATH:$HOME/.local"
+setxkbmap pl
+xrandr --output DVI-D-0 --left-of HDMI-0 --auto --rate 75
+xrandr --output HDMI-0 --auto --rate 75
+feh --bg-scale $_here/wallpaper.*
+pkill -USR1 -x sxhkd; sxhkd &
+pipewire &
+pamixer --allow-boost &
+eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh) &
+#picom -b 
+. $_here/functions
+xsetroot_gen &
