217fb24a354497326c04c35192c56ad73d55a623
Ohio2 <sekmartyna4@gmail.com>
2022-05-01 18:06:22 +0200

    nvim

---
+++ .config/lua/vapour/user-config/theme/init.lua
@@ -0,0 +1,4 @@
+Vapour.utils.plugins.add_user({'tomasisier/vim-code-dark'})
+Vapour.utils.plugins.add_user({'nacro90/numb.nvim', {config = function() require'numb'.setup{show_numbers = false} end}})
+
+Vapour.settings.colorscheme = 'vim-code-dark'
