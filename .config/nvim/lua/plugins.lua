21a97ebe2eb533e0089a0f856e5c80e1e4a7b3ba
Ohio2 <sekmartyna4@gmail.com>
2022-05-05 21:16:36 +0200

    e

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,52 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use {
+    'haorenW1025/completion-nvim',
+    opt = true,
+    requires = {{'hrsh7th/vim-vsnip', opt = true}, {'hrsh7th/vim-vsnip-integ', opt = true}}
+  }
+
+  -- You can specify rocks in isolation
+  use_rocks 'penlight'
+  use_rocks {'lua-resty-http', 'lpeg'}
+
+    -- Post-install/update hook with neovim command
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+  -- Post-install/update hook with call of vimscript function with argument
+  use { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end }
+  -- Use dependency and run lua function after load
+  use {
+    'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
+    config = function() require('gitsigns').setup() end
+  }
+  use {"akinsho/toggleterm.nvim"}
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    }
+	}
+  use 'ms-jpq/coq_nvim'
+  use 'tomasiser/vim-code-dark'
+  use 'andweeb/presence.nvim'
+  use {
+  "folke/which-key.nvim",
+  config = function()
+    require("which-key").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+  }
+  use {"romgrk/barbar.nvim"}
+  use 'hrsh7th/nvim-cmp'
+  use 'windwp/nvim-autopairs'
+  use 'ntpeters/vim-better-whitespace'
+ end)
